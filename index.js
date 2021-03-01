475ac665b5aea433f8ca3204a296ddf08e250903
Matthew <cameron.matt@outlook.com>
2021-03-01 22:09:41 +1100

    Done Calculator Challenge

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b){
+  return (a + b);
+}
+
+function subtract(b, a){
+  return (b - a);
+}
+
+function multiply(a, b){
+  return (a * b);
+}
+
+function divide(a, b){
+  return (a / b);
+}
+
+function increment(n){
+  return (n + 1);
+}
+
+function decrement(n){
+  return (n - 1);
+}
+
+function makeInt(n){
+  return parseInt(n ,10);
+}
+
+function assume(n){
+  return (n =10);
+}
+
+function preserveDecimal(n){
+return parseInt(n);
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
